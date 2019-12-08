939d41e4ce8dad14d39554d0f7eec8642d135d9b
david.g <davidgilady@hotmail.com>
2019-12-08 13:14:05 +0200

    fixed enable file sharing recipe so it will work even when the networking module was not installed before. Added vagrant kitchen driver

--- chef-windows/recipes/enable_file_sharing.rb
+++ chef-windows/recipes/enable_file_sharing.rb
@@ -1,3 +1,8 @@
+powershell_script "install xNetworking module" do
+    code "Install-Module xNetworking -Force"
+    not_if "(Get-Module xNetworking -list) -ne $null"
+end
+
 dsc_resource "File sharing firewall rule (SMB-In)" do
   resource :xfirewall
   property :name, "File and Printer Sharing (SMB-In)"
